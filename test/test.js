25abba80a213aa24bf595130a0cb1a10570de255
Jan Erik Hanssen <jhanssen@gmail.com>
2018-02-27 01:04:39 -0800

    The example works on raw audio, not wav files

--- test/test.js
+++ test/test.js
@@ -2,10 +2,10 @@
 const { ResampleStream } = require("..");
 
 const fs = require("fs");
-const rs = fs.createReadStream("testin.wav");
+const rs = fs.createReadStream("testin.raw");
 const tr = new ResampleStream({
     sourceFormat: { format: "s16", rate: 44100, channels: 2 },
     destinationFormat: { format: "s16", rate: 8000, channels: 1 }
 });
-const ws = fs.createWriteStream("testout2.wav");
+const ws = fs.createWriteStream("testout2.raw");
 rs.pipe(tr).pipe(ws);
